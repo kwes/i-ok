06d46e84709d3561f0e4aa43e5890d1eca690e4a
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2021-05-23 15:14:36 -0400

    Update iok.py

--- iok.py
+++ iok.py
@@ -3,9 +3,10 @@ def run():
   file = open(file, "r")
   script = file.read()
   file.close()
+  
   script = script.split(" ")
   res = 0
-  a = 16
+  
   for char in script:
     if char == "i+":
       res += 1
@@ -14,51 +15,17 @@ def run():
     elif char == "i*":
       res *= 2
     elif char == "i/":
-      res /= 2
+      res //= 2
      elif char == "i--":
       res -= 2
     elif char == "i++":
       res += 2
-  if res == a:
-    res = "a"
-  elif res == a + 1:
-    res = "b"
-  elif res == a + 2:
-    res = "c"
-  elif res == a + 3:
-    res = "d"
-  elif res == a + 4:
-    res = "e"
-  elif res == a + 5:
-    res = "f"
-  elif res == a + 6:
-    res = "g"
-  elif res == a + 7:
-    res = "h"
-  elif res == a + 8:
-    res = "i"
-  elif res >= a + 9:
+  
+  if res > 15 and res < 25:
+    res = chr(res + 81)
+  elif res < -15 and res > -26:
+    res = "!@#$%^&*()"[-res - 16]
+  elif res > 24 or res < -25:
     res = "null"
-  elif res == ps: #special chars
-    res = "!"
-  elif res == ps - 1:
-    res = "@"
-  elif res == ps - 2:
-    res = "#"
-  elif res == ps - 3:
-    res = "$"
-  elif res == ps - 4:
-    res = "%"
-  elif res == ps - 5:
-    res = "^"
-  elif res == ps - 6:
-    res = "&"
-  elif res == ps - 7:
-    res = "*"
-  elif res == ps - 8:
-    res = "("
-  elif res == ps - 9:
-    res = ")"
-  elif res <= ps - 10:
-    res = "null"
+  
   print(res)
